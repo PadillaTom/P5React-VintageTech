68367156e3ff055b5fa85cd5a80c79256760c533
Tomas Padilla <padillatomasagustin@gmail.com>
2020-09-10 10:09:35 +0200

    Done Context API and STATE, now AXIOS and URLS

--- src/context/products.js
+++ src/context/products.js
@@ -1,1 +1,26 @@
-// products context
+// :::::::::::::: Products CONTEXT ::::::::::::::
+import React from 'react';
+
+// Creating Context:
+export const ProductContext = React.createContext();
+// Provider - Consumer - useContext() --> We don't need anymore the CONSUMER.
+// We set it up directly on the Return.
+//
+// -----> Enviar los CHILDREN as Prop
+// -----> Value Prop = Seleccionamos que queremos enviar: Object
+export default function ProductProvider({ children }) {
+  // Ejemplo para usar el Context: //
+  // const greeting = 'Hello';
+  // const product = { id: 1, title: 'Product Name' };
+  // End Ejemplo para usar el context //
+  //
+  //  React UseState() --> [value, Function to use them]
+  const [loading, setLoading] = React.useState(false);
+  const [products, setProducts] = React.useState([]);
+  const [featured, setFeatured] = React.useState([]);
+  return (
+    <ProductContext.Provider value={{ products, loading, featured }}>
+      {children}
+    </ProductContext.Provider>
+  );
+}
--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,5 +1,15 @@
-import React from "react";
+import React, { useContext } from 'react';
+import { ProductContext } from '../context/products';
 
+// Main
 export default function Products() {
+  // Using the CONTEXT (ProductContext) creado en products.js
+  // console.log(React.useContext(ProductContext)); // Vemos que nos muestra directamente el VALUE! (Data que pasamos en Value)
+  // Ejemplo para usar el Context : //
+  // const { greeting } = React.useContext(ProductContext);
+  // console.log(greeting);
+  // End Ejemplo para usar el context //
+  const { loading, products } = React.useContext(ProductContext);
+
   return <h1>hello from products page</h1>;
 }
